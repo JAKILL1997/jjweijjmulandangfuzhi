fe8365ab5bec0e93d2926552e1a194109aa20b2d
王丹 <dan.wang@husor.com>
2017-02-21 09:51:02 +0800

    添加相关代码实现

---
+++ APPTranslate/APPTranslate/NSString+MD5.h
@@ -0,0 +1,19 @@
+//
+//  NSString+MD5.h
+//  APPTranslate
+//
+//  Created by wangdan on 2017/2/21.
+//  Copyright © 2017年 王丹. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString(MD5)
+
+- (NSString *)jk_md5;
+
+- (NSString *)jk_urlEncode;
+
+- (NSString *)jk_urlDecode;
+
+@end
---
+++ APPTranslate/APPTranslate/NSString+MD5.m
@@ -0,0 +1,53 @@
+//
+//  NSString+MD5.m
+//  APPTranslate
+//
+//  Created by wangdan on 2017/2/21.
+//  Copyright © 2017年 王丹. All rights reserved.
+//
+
+#import "NSString+MD5.h"
+#import <CommonCrypto/CommonCrypto.h>
+
+#define CC_MD5_DIGEST_LENGTH    16          /* digest length in bytes */
+#define CC_MD5_BLOCK_BYTES      64          /* block size in bytes */
+#define CC_MD5_BLOCK_LONG       (CC_MD5_BLOCK_BYTES / sizeof(CC_LONG))
+
+@implementation NSString(MD5)
+
+- (NSString *)jk_md5 {
+    const char* input = [self UTF8String];
+    unsigned char result[CC_MD5_DIGEST_LENGTH];
+    CC_MD5(input, (CC_LONG)strlen(input), result);
+    NSMutableString *digest = [NSMutableString stringWithCapacity:CC_MD5_DIGEST_LENGTH * 2];
+    for (NSInteger i = 0; i < CC_MD5_DIGEST_LENGTH; i++) {
+        [digest appendFormat:@"%02x", result[i]];
+    }
+    return digest;
+    
+}
+
+- (NSString *)jk_urlEncode {
+    NSString *unencodedString = self;
+    NSString *encodedString = (NSString *)
+    CFBridgingRelease(CFURLCreateStringByAddingPercentEscapes(kCFAllocatorDefault,
+                                                              (CFStringRef)unencodedString,
+                                                              NULL,
+                                                              (CFStringRef)@"!*'();:@&=+$,/?%#[]",
+                                                              kCFStringEncodingUTF8));
+    
+    return encodedString;
+}
+
+- (NSString *)jk_urlDecode {
+    NSString *encodedString = self;
+    NSString *decodedString  = (__bridge_transfer NSString *)CFURLCreateStringByReplacingPercentEscapesUsingEncoding(NULL,
+                                                                                                                     (__bridge CFStringRef)encodedString,
+                                                                                                                     CFSTR(""),
+                                                                                                                     CFStringConvertNSStringEncodingToEncoding(NSUTF8StringEncoding));
+    return decodedString;
+}
+
+
+
+@end
